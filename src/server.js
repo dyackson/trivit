f429cf299caec626c7d64e2db33038de49140f54
Jackson Millsaps <jmillsaps@datatrac.com>
2019-07-07 20:43:00 -0400

    add mongoose

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import {connect} from './connect_db';
+
+connect();
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
