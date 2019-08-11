086d67456fdf1d51a4d2047243ecd0654891df04
Jackson Millsaps <jmillsaps@datatrac.com>
2019-08-11 13:58:21 -0400

    work on question validator

--- src/get_valid_copy_of_question.js
+++ src/get_valid_copy_of_question.js
@@ -33,63 +33,66 @@ export default function get_valid_copy_of_question(question) {
 
 
 
-function get_valid_type(type) {
-    if (!VALID_TYPES.contains(type)) {
-        throw Error(`unknown type: ${type}`);
+export function get_valid_type(type) {
+    if (!VALID_TYPES.includes(type)) {
+        throw new InvalidQuestion(`unknown type: ${type}`);
     }
     return type;
 }
 
-function get_valid_prompt(prompt) {
+export function get_valid_prompt(prompt) {
     if (typeof prompt !== 'string') {
-        throw Error(`prompt must be string: ${prompt}`);
+        throw new InvalidQuestion(`prompt must be string: ${prompt}`);
     }
 
     const trimmed = prompt.trim();
 
-    if (trimmed.length) {
-        throw Error(`prompt is empty: ${prompt}`);
+    if (!trimmed.length) {
+        throw new InvalidQuestion(`prompt is empty: ${prompt}`);
     }
 
     return trimmed;
 }
 
-function get_valid_choices({choices, type}) {
+export function get_valid_choices({choices, type}) {
     if (type === 'true_false') {
         return;
     } else {
         const choices_is_empty = !Array.isArray(choices) || !choices.length;
         if (choices_is_empty) {
-            throw Error(`choices must be a non-empty array: ${choices}`);
+            throw new InvalidQuestion(
+                `choices must be a non-empty array: ${choices}`);
         }
 
         const trimmed_choices = choices.map((choice, i) => {
-            if (typeof choice === 'string') {
-                throw Error(`choice ${i} is not a string: ${choice}`);
+            if (typeof choice !== 'string') {
+                throw new InvalidQuestion(
+                    `choice ${i} is not a string: ${choice}`);
             }
             const trimmed = choice.trim();
             if (!trimmed.length) {
-                throw Error(`choice ${i} an empty string: ${choice}`);
+                throw new InvalidQuestion(
+                    `choice ${i} an empty string: ${choice}`);
             }
             return trimmed;
         })
 
-        const choice_set = new Set(choices);
-        if (choices.length < choice_set.size) {
-            throw Error(`multiple choices are identical`);
+        const choice_set = new Set(trimmed_choices);
+        if (choices.length !== choice_set.size) {
+            throw new InvalidQuestion(`multiple choices are identical`);
         }
 
         return trimmed_choices;
     }
 }
 
-function get_valid_answer({answer, choices, type}) {
+export function get_valid_answer({answer, choices, type}) {
     const validators_by_type = {
     };
 }
 
-function get_valid_tags(tags) {
+export function get_valid_tags(tags) {
 }
 
-function get_valid_links(links) {
+export function get_valid_links(links) {
 }
--- test/get_valid_copy_of_question.js
+++ test/get_valid_copy_of_question.js
@@ -1,21 +1,89 @@
 /* global it, describe */
-import get_valid_copy_of_question from '@/get_valid_copy_of_question';
+import get_valid_copy_of_question, {
+    get_valid_type,
+    get_valid_prompt,
+    get_valid_choices,
+    get_valid_answer,
+    get_valid_tags,
+    get_valid_links,
+    VALID_TYPES,
+} from '@/get_valid_copy_of_question';
 import InvalidQuestion from '@/InvalidQuestion';
 
 import expect from 'expect';
 
 describe(`get_valid_copy_of_question`, () => {
-    it(`errors if 'type' is missing`, () => {
-        expect(() => get_valid_copy_of_question({}))
-            .toThrow(InvalidQuestion);
+    describe(`get_valid_type`, () => {
+        const errors_if = [
+            [`type is missing`, undefined],
+            [`type is not a string`, 4],
+            [`type is unknown`, 'foo'],
+        ];
+        errors_if.forEach(([msg, type]) => {
+            it(msg, () => {
+                expect(() => get_valid_type(type))
+                    .toThrow(InvalidQuestion)
+            });
+        });
+
+        it('returns the type if in valid types', () => {
+            const type = VALID_TYPES[0];
+            expect(get_valid_type(type)).toEqual(type);
+        });
     });
-    it(`errors if 'type' is not a string`, () => {
-        expect(() => get_valid_copy_of_question({type: 4}))
-            .toThrow(InvalidQuestion);
+    describe(`get_valid_prompt`, () => {
+        const errors_if = [
+            [`prompt is missing`, undefined],
+            [`prompt is not a string`, 4],
+            [`prompt is whitespace`, ' \t'],
+        ];
+        errors_if.forEach(([msg, prompt]) => {
+            it(msg, () => {
+                expect(() => get_valid_prompt(prompt))
+                    .toThrow(InvalidQuestion)
+            });
+        });
+
+        it('returns the trimmed prompt if valid', () => {
+            expect(get_valid_prompt(' why ')).toEqual('why');
+        });
+
     });
-    it('errors if type is unknown', () => {
-        expect(() => get_valid_copy_of_question({type: 'foo'}))
-            .toThrow(InvalidQuestion);
+    describe(`get_valid_choices`, () => {
+        const errors_if = [
+            [`type not true_false and choices is missing`,
+                {type: 'foo'}],
+            [`choices is not an array`,
+                {type: 'foo', choices: 4}],
+            [`choices is an empty array`,
+                {type: 'foo', choices: []}],
+            [`choices contains a non-string item`,
+                {type: 'foo', choices: ['x', 2]}],
+            [`choices contains a whitespace item`,
+                {type: 'foo', choices: ['x', ' \t']}],
+            [`choices contains contains duplicates`,
+                {type: 'foo', choices: ['x', 'y', 'x ']}],
+        ];
+        errors_if.forEach(([msg, input]) => {
+            it(msg, () => {
+                expect(() => get_valid_choices(input))
+                    .toThrow(InvalidQuestion)
+            });
+        });
+
+        it('returns undefined if type is true_false', () => {
+            expect(get_valid_choices({type: 'true_false'})).toEqual(undefined);
+        });
+        it('returns the trimmed choices otherwise', () => {
+            expect(get_valid_choices({type: 'foo', choices: ['\t x', 'y']}))
+                .toEqual(['x', 'y']);
+        });
+    });
+    describe(`get_valid_answer`, () => {
+    });
+    describe(`get_valid_tags`, () => {
+    });
+    describe(`get_valid_links`, () => {
     });
 
 });
