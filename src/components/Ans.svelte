03d729dad71e751c2da107db8f702d6abb1ebfd8
Jackson Millsaps <jmillsaps@datatrac.com>
2019-09-21 22:05:19 -0400

    revert Ans.svelte since it wont be used for guesses

--- src/components/Ans.svelte
+++ src/components/Ans.svelte
@@ -5,58 +5,28 @@
     export let value = false;
     export let delete_ans = null;
     export let toggle = null;
-    export let play_mode = false;
-
-    let icon;
-    let icon_color = '';
-
-    $: {value;
-        set_icon();
-    }
-
-    function set_icon() {
-        if (play_mode) {
-            icon = value ? 'check_circle_outline' : 'radio_button_unchecked';
-            // no color in play mode
-        } else {
-            icon_color = value ? 'is-success' : 'is-danger';
-            icon = value ? 'thumb_up_alt' : 'thumb_down_alt';
-        }
-    }
+    $: left_icon = value ? 'thumb_up_alt' : 'thumb_down_alt';
+    $: color = value ? 'is-success' : 'is-danger';
 
 
 </script>
 
-{#if play_mode}
-    <div class="button on:click={toggle} {icon_color}">
-        <span class="icon is-small">
-            <i class='material-icons'>{icon}</i>
-        </span>
-        <span>{text}</span>
-    </div>
-{:else}
 <div class="field has-addons">
     <div class=control on:click={toggle}>
-        <div class="button {icon_color}">
+        <div class="button {color}">
             <span class="icon is-small">
-                <i class='material-icons'>{icon}</i>
+                <i class='material-icons'>{left_icon}</i>
             </span>
         </div>
     </div>
     <div class="control">
-        <input
-            class='input'
-            type=text
-            {placeholder}
-            bind:value={text}
-            readonly={play_mode}>
+        <input class='input' type=text {placeholder} bind:value={text}>
     </div>
-        <div class=control on:click={delete_ans}>
-            <div class="button">
-                <span class="icon is-small">
-                    <i class='material-icons'>clear</i>
-                </span>
-            </div>
+    <div class=control on:click={delete_ans}>
+        <div class="button">
+            <span class="icon is-small">
+                <i class='material-icons'>clear</i>
+            </span>
         </div>
+    </div>
 </div>
-{/if}
