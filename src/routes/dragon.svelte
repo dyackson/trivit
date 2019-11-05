c2d2dcd897e112eb75ca96539ce8ccfb46bfdd12
yackSpillsaps <jacksonkmillsaps@gmail.com>
2019-11-05 09:40:26 -0500

    begin fixing space between items

--- src/routes/dragon.svelte
+++ src/routes/dragon.svelte
@@ -22,10 +22,28 @@
     // dragenter required on mobile, per
     // github.com/timruffles/mobile-drag-drop#polyfill-requires-dragenter-listener
     function obligatoryHandler() {}
+
+    function onDragOverSite(index) {
+        console.log('dragged over', index);
+        potentialDropIndex = index;
+    }
+
+    function onDragEnterSite(index) {
+        console.log('drag entered', index);
+        potentialDropIndex = index;
+    }
+
+    function onDragLeaveSite(index) {
+        console.log('drag left', index);
+        potentialDropIndex = null;
+    }
 </script>
 
 <style>
-    div {
+    .wrapper {
+    }
+
+    .item {
         width: fit-content;
         border: 2px solid white;
         margin: .5em;
@@ -35,14 +53,18 @@
         padding: 4em;
     }
 
-    .drop-site {
+    .space-between-item {
+        border: 2px solid white;
         width: 3em;
+        position: relative;
+        top: 2em;
     }
     .dragged-over {
         width: 6em;
     }
 </style>
 
+<!--
 <div id='dragon' draggable=true on:dragstart={onDragStart}>Dragon</div>
 <br>
 <div id='target'
@@ -52,15 +74,19 @@
     >
     Target
 </div>
+-->
 
 {#each items as item, index (item)}
-    <div draggable=true>item</div>
-    {#if index !== items.length - 1}
-    <div
-        class=drop-site
-        class:dragged-over={index === potentialDropIndex}
-        on:dragenter|preventDefault={() => setPotentialDragIndex(index)}
-        on:dragleave|preventDefault={() => setPotentialDragIndex(null)}
-        ></div>
-    {/if}
+    <div class=wrapper>
+        <div class=item draggable=true>item</div>
+        {#if index !== items.length - 1}
+        <div
+            class=space-between-item
+            class:dragged-over={index === potentialDropIndex}
+            on:dragenter|preventDefault={() => onDragEnterSite(index)}
+            on:dragover|preventDefault={() => true || onDragOverSite(index)}
+            on:dragleave|preventDefault={() => onDragLeaveSite(index)}
+            ></div>
+        {/if}
+    </div>
 {/each}
