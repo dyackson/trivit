a86c9c691aadd8f9e378045cd59384cf3bef0e1d
yack <jacksonkmillsaps@gmail.com>
2020-08-17 16:16:48 -0400

    make navbar open/close

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -41,70 +41,6 @@
 
 <style>
 
-    nav {
-        font-size: calc(2vw + .5em);
-        height: 2em;
-    }
-
-    #menu-button {
-        position: relative;
-        top: 1em;
-        left: 1em;
-        cursor: pointer;
-        z-index: 2;
-        width: fit-content;
-        transform: translate(-4em, 0);
-        transition: transform 0.5s var(--ttf);
-    }
-
-    #menu-button.open {
-        color: var(--dark);
-        transform: none;
-        transition: color, transform 0.5s var(--ttf);
-    }
-
-    #menu {
-        background: var(--light);
-        color: var(--dark);
-        position: absolute;
-        padding: 1em;
-        padding-top: 3em;
-        top: 0;
-        margin: 0;
-
-        border-color: white;
-        border-width: 2px;
-        list-style-type: none;
-        /* to stop flickering of text in safari */
-        -webkit-font-smoothing: antialiased;
-        transform: translate(-110%, 0);
-        transition: transform 0.5s var(--ttf);
-        z-index: 1;
-        border-bottom-right-radius: 5em;
-    }
-
-    #menu.open {
-        transform: none;
-    }
-
-    a {
-        text-decoration: none;
-        color: var(--dark);
-    }
-
-    a:hover {
-        color: tomato;
-    }
-
-    li {
-        list-style: none;
-        font-size: 1.5em;
-        margin-bottom: .5em;
-    }
-
-    a:last-of-type > li {
-        margin-bottom: 0;
-    }
 </style>
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
@@ -112,7 +48,13 @@
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
     </a-->
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button"
+           on:click|preventDefault={() => open = !open}
+           class="navbar-burger burger"
+           aria-label="menu"
+           aria-expanded={open}
+           data-target="navbar"
+           >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -122,7 +64,9 @@
   <!-- the is-active class is what opens and closes it on mobile
        We need to close it when it's clicked
     -->
-  <div id="navbarBasicExample" class="navbar-menu is-active">
+  <div id="navbar" class="navbar-menu"
+    class:is-active={open}
+       >
     <div class="navbar-start">
         {#each routes as {href, display}}
             <a class=navbar-item
